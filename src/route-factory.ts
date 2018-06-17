c3b4cca1a524ff9b477f83544ff65735720d6ce1
Maciej Chałapuk <maciej@chalapuk.pl>
2018-06-17 23:31:58 +0200

    ~ route factory uses new model

--- src/route-factory.ts
+++ src/route-factory.ts
@@ -1,7 +1,7 @@
 import { createElement, ReactElement } from 'react';
 import { Route, RouteProps } from 'react-router-dom';
 
-import { Website, Page } from './models';
+import { Paramorph, Page } from './model';
 
 const NOT_FOUND_URL = '/404';
 
@@ -11,41 +11,26 @@ export interface PageWithRoute {
 }
 
 export class RoutesFactory {
-  getRoutes(website : Website) : PageWithRoute[] {
-    var error404 = website.pages[NOT_FOUND_URL];
+  getRoutes(paramorph : Paramorph) : PageWithRoute[] {
+    var error404 = paramorph.pages[NOT_FOUND_URL];
     if (error404 === undefined) {
       throw new Error(`couldn't find page of url ${NOT_FOUND_URL}`);
     }
 
     function createRoute(page : Page, path = page.url, exact = true) : PageWithRoute {
-      const component = () => createElement(page.layout.component as any, { website, page });
+      const component = () => createElement(page.layout.component as any, { paramorph, page });
       const routeProps = { page, path, exact, key: path, component };
       const route = createElement(Route, routeProps);
       return { page, route };
     }
 
-    const routes = [].concat.call(
-      // categories
-      Object.keys(website.categories)
-        .map(title => website.categories[title])
-        .filter(category => category.output)
-        .map(category => createRoute(category)),
-
-      // tags
-      Object.keys(website.tags)
-        .map(title => createRoute(website.tags[title])),
-
-      // pages
-      Object.keys(website.pages)
-        .map(title => website.pages[title])
-        .filter(page => page.output)
-        .map(page => createRoute(page)),
-
-      // 404 with exact = false (must be at the end)
-      [
-        createRoute(error404, '/', false),
-      ],
-    );
+    const routes = Object.keys(paramorph.pages)
+      .map(url => paramorph.pages[url])
+      .filter(page => page.output)
+      .map(page => createRoute(page))
+    ;
+    // 404 with exact = false (must be at the end)
+    createRoute(error404, '/', false);
 
     return routes;
   }
