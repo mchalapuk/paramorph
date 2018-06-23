895bca291fa9704a96e18b2dac93abac2d11cc65
Maciej Chałapuk <maciej@chalapuk.pl>
2018-06-23 11:33:08 +0200

    ~ universal-router-based route factory

--- src/route-factory.ts
+++ src/route-factory.ts
@@ -1,36 +1,32 @@
 import { createElement, ReactElement } from 'react';
-import { Route, RouteProps } from 'react-router-dom';
+import { Route, Params, Context } from 'universal-router';
 
-import { Paramorph, Page } from './model';
+import { Paramorph, Page, ComponentType } from './model';
 
 const NOT_FOUND_URL = '/404';
 
-export interface PageWithRoute {
-  page : Page;
-  route : ReactElement<RouteProps>;
-}
-
 export class RoutesFactory {
-  getRoutes(paramorph : Paramorph) : PageWithRoute[] {
+  getRoutes(paramorph : Paramorph) : Route[] {
     var error404 = paramorph.pages[NOT_FOUND_URL];
     if (error404 === undefined) {
       throw new Error(`couldn't find page of url ${NOT_FOUND_URL}`);
     }
 
-    function createRoute(page : Page, path = page.url, exact = true) : PageWithRoute {
-      const component = () => createElement(page.layout.component as any, { paramorph, page });
-      const routeProps = { page, path, exact, key: path, component };
-      const route = createElement(Route, routeProps);
-      return { page, route };
+    function createRoute(page : Page, path = page.url) : Route {
+      return { path, action: async (context : Context, params : Params) => {
+        const layout = await paramorph.loadLayout(page.layout);
+        const component = await paramorph.loadPage(path);
+        return (() => createElement(layout, { component, page, paramorph })) as ComponentType;
+      } };
     }
 
     const routes = Object.keys(paramorph.pages)
-      .map(url => paramorph.pages[url])
+      .map(url => paramorph.pages[url] as Page)
       .filter(page => page.output)
       .map(page => createRoute(page))
     ;
-    // 404 with exact = false (must be at the end)
-    createRoute(error404, '/', false);
+    // 404 (must be at the end)
+    createRoute(error404, '/:anything');
 
     return routes;
   }
