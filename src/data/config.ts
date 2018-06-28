bf6c32db37f4921d65dede7fbc3f543bcef953b2
Maciej Chałapuk <maciej@chalapuk.pl>
2018-06-28 04:11:02 +0200

    - most of previous implementation

--- src/data/config.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'fs';
-import { safeLoad } from 'js-yaml';
-
-const config = safeLoad(readFileSync('./_config.yml', 'utf8'));
-const code = `module.exports = ${JSON.stringify(config)};\n`;
-
-module.exports = () => ({ code });
-
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,57 +1,9 @@
-import { createElement } from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
-
 import { stripTags } from '../utils';
-
-import { Page, Category, Tag, Collection, Layout, Include, Website, HashTable } from '../models';
-
-import layouts from './layouts';
-import includes from './includes';
-import collections from './collections';
-import pages from './pages';
-import categories from './categories';
-import tags from './tags';
-import menu from './menu';
-
-const config = require('./config');
-
-const website = new Website(
-  checkIsString(config.title, 'config.title'),
-  checkIsString(config.baseUrl, 'config.baseUrl'),
-  checkIsString(config.timezone, 'config.timezone'),
-  checkIsString(config.locale || 'en_US', 'config.locale'),
-);
-
-export default website;
-
-layouts.forEach((layout : Layout) => website.addLayout(layout));
-includes.forEach((include : Include) => website.addInclude(include));
-collections.forEach((collection : Collection) => website.addCollection(collection));
-pages.forEach((page : Page) => website.addPage(page));
-categories.forEach((category : Category) => website.addCategory(category));
-tags.forEach((tag : Tag) => website.addTag(tag));
-website.menu = menu;
 
 const index = website.pages['/'];
 if (index === undefined) {
   throw new Error('page of url \'/\' must be defined to create index.html');
 }
-
-// add pages to categories and tags
-pages
-  .filter((page : Page) => page.feed && page.output)
-  .forEach((page : Page) => {
-    const requiredBy = `pages['${page.url}']`;
-    page.categories.forEach(title => website.getCategoryOfTitle(title, requiredBy).pages.push(page));
-    page.tags.forEach(title => website.getTagOfTitle(title, requiredBy).pages.push(page));
-  });
-
-// add sub-categories to categories
-categories.forEach((page : Page) => {
-  const requiredBy = `pages['${page.url}']`;
-  page.categories.forEach(title => website.getCategoryOfTitle(title, requiredBy).pages.push(page));
-});
 
 // generate descriptions for pages, categories and tags
 pages.forEach((page : Page) => {
@@ -116,13 +68,6 @@ function imageFromContent(page : Page) {
     return null;
   }
   return found[1];
-}
-
-function checkIsString(value : any, name : string) {
-  if (typeof value !== 'string') {
-    throw new Error(`${name} must be a string; got ${value} (${typeof value})`);
-  }
-  return value as string;
 }
 
 function removeEntities(str : string) {
