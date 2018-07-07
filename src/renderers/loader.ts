c34b781cd422e11438139de62dbb8c595850f4d1
Maciej Chałapuk <maciej@chalapuk.pl>
2018-07-07 03:22:30 +0200

    ~ reimplemented LoaderRenderer to fetch sources via webpack and cook them in node vm

--- src/renderers/loader.ts
+++ src/renderers/loader.ts
@@ -2,6 +2,7 @@
 import { ComponentType, ReactElement, createElement } from 'react';
 import { renderToStaticMarkup, renderToString } from 'react-dom/server';
 import { History } from 'history';
+import { Script } from 'vm';
 
 import { Paramorph, Page } from '../model';
 import { ContextContainer } from './ContextContainer';
@@ -9,23 +10,74 @@ import { ContextContainer } from './ContextContainer';
 export class LoaderRenderer {
   constructor(
     private history : History,
-    private paramorph : Paramorph
+    private paramorph : Paramorph,
+    private loadPage : (request : string, callback : LoadCallback) => void,
   ) {
   }
 
-  render(page : Page) : string {
+  async render(page : Page) : Promise<string> {
+    const request = `@website${page.source.substring(1)}`;
+    const component = await this.loadComponent(request);
+    const element = createElement(component, { respectLimit: false });
+
     const props = {
       history: this.history,
       paramorph: this.paramorph,
       page,
-      respectLimit: false,
     };
-    const component = require(`@website${page.source.substring(1)}`).default;
+    const container = createElement(ContextContainer, props, element);
 
-    const element = createElement(component, props);
     return renderToStaticMarkup(element);
+  }
+
+  private loadComponent(request : string) : Promise<ComponentType> {
+    return new Promise((resolve, reject) => {
+      this.loadPage(request, (err, source, sourceMap, module) => {
+        if (err) {
+          reject(err);
+        }
+        const componentModule = this.eval(request, source);
+        if (!componentModule.hasOwnProperty('default')) {
+          reject(new Error(`Module '${request} must contain a default export...'`));
+        }
+        resolve(componentModule.default);
+      });
+    });
+  }
+
+  private eval(filename : string, source : string) {
+    const sandbox = {} as any;
+
+    const exports = {} as any;
+    sandbox.exports = exports;
+
+    sandbox.module = {
+      exports: exports,
+      filename: filename,
+      id: filename,
+      parent: module.parent,
+      require: sandbox.require || requireLike(filename),
+    };
+    sandbox.global = sandbox;
+
+    const options = {
+      filename: filename,
+      displayErrors: true,
+    }
+
+    const script = new vm.Script(source, options);
+    script.runInNewContext(sandbox, options)
+
+    return sandbox.module.exports;
   }
 }
 
 export default LoaderRenderer;
+
+export type LoadeCallback = (
+  err : any,
+  source: string,
+  sourceMap : string,
+  module : NormalModule,
+) => void;
 
