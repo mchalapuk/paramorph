9c04bde111ece7b4e14d175f53fd44320ab55799
Maciej Chałapuk <maciej@chalapuk.pl>
2019-02-05 01:26:52 +0100

    ~ fixed exports in default file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
 
-export * from './model';
-export * from './react';
+export {
+  Config,
+  Layout,
+  Include,
+  Page,
+  Category,
+  Tag,
+  Paramorph,
+  ComponentType,
+} from './model';
+
+export {
+  Context,
+  ContextTypes,
+  ContextContainer,
+  Component,
+  PureComponent,
+  Link,
+  DeferredStyles,
+  DeferredScripts,
+  NodeMapper,
+  Root,
+  RootProps,
+  BundleUrls,
+} from './react';
 
