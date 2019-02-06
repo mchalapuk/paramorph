cc303de2c3d863eb148e707707aeb3a1355468dc
Maciej Chałapuk <maciej@chalapuk.pl>
2019-02-06 01:11:23 +0100

    ~ separated loader logic from webpack's boilerplace in MarkdownLoader

--- src/loader/markdown/MarkdownLoader.ts
+++ src/loader/markdown/MarkdownLoader.ts
@@ -1,57 +1,25 @@
 
-import * as webpack from 'webpack';
-import Module = require('module');
+import { Paramorph } from '../../model';
 
 import MarkdownCompiler from './MarkdownCompiler';
 import ComponentTemplate from './ComponentTemplate';
 import TypeScriptCompiler from './TypeScriptCompiler';
 
-const markdown = new MarkdownCompiler();
-const template = new ComponentTemplate();
-const typescript = new TypeScriptCompiler();
-
-export function MarkdownLoader(this : webpack.loader.LoaderContext, source : string) {
-  this.cacheable && this.cacheable();
-  const callback = this.async();
-
-  if (!callback) {
-    throw new Error('MarkdownLoader: couldn\'t create callback.');
+export class MarkdownLoader {
+  constructor(
+    private readonly markdown = new MarkdownCompiler(),
+    private readonly template = new ComponentTemplate(),
+    private readonly typescript = new TypeScriptCompiler(),
+  ) {
   }
-  const configUrl = this.resolve(this.context, '@website/_config', (err, configUrl) => {
-    if (err) {
-      this.emitError(err);
-      return;
-    }
 
-    this.loadModule(configUrl, (err, configSource) => {
-      if (err) {
-        this.emitError(err);
-        return;
-      }
-
-      try {
-        const paramorph = exec.call(this, configSource, '_config.yml');
-
-        const html = markdown.toHtml(source, this.resourcePath);
-        const tsSource = template.compile(html, paramorph);
-        const output = typescript.compile(tsSource, this.resourcePath);
-
-        callback(null, output);
-
-      } catch (e) {
-        callback(err);
-      }
-    });
-  });
+  load(source : string, fileName : string, paramorph : Paramorph) {
+    const html = this.markdown.toHtml(source, fileName);
+    const tsSource = this.template.compile(html, paramorph);
+    const output = this.typescript.compile(tsSource, fileName);
+    return output;
+  }
 };
 
 export default MarkdownLoader;
-
-function exec(this : webpack.loader.LoaderContext, source : string, url : string) {
-  const module = new Module(url, this as any);
-  module.paths = (Module as any)._nodeModulePaths(this.context);
-  module.filename = url;
-  (module as any)._compile(source, url);
-  return module.exports.default;
-}
 
--- src/loader/markdown/index.ts
+++ src/loader/markdown/index.ts
@@ -1,5 +1,49 @@
 
-import { MarkdownLoader } from './MarkdownLoader';
+import * as webpack from 'webpack';
+import Module = require('module');
 
-export = MarkdownLoader;
+import MarkdownLoader from './MarkdownLoader';
+
+export = markdownLoader;
+
+function markdownLoader(this : webpack.loader.LoaderContext, source : string) {
+  this.cacheable && this.cacheable();
+  const callback = this.async();
+
+  if (!callback) {
+    throw new Error('MarkdownLoader: couldn\'t create callback.');
+  }
+  const configUrl = this.resolve(this.context, '@website/_config', (err, configUrl) => {
+    if (err) {
+      this.emitError(err);
+      return;
+    }
+
+    this.loadModule(configUrl, (err, configSource) => {
+      if (err) {
+        this.emitError(err);
+        return;
+      }
+
+      try {
+        const loader = new MarkdownLoader();
+        const paramorph = exec.call(this, configSource, '_config.yml');
+
+        const output = loader.load(source, this.resourcePath, paramorph);
+
+        callback(null, output);
+      } catch (e) {
+        callback(err);
+      }
+    });
+  });
+};
+
+function exec(this : webpack.loader.LoaderContext, source : string, url : string) {
+  const module = new Module(url, this as any);
+  module.paths = (Module as any)._nodeModulePaths(this.context);
+  module.filename = url;
+  (module as any)._compile(source, url);
+  return module.exports.default;
+}
 
