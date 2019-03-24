4aa5dc98ab28be08215f23d0eea7bc1dd4804659
Maciej Chałapuk <maciej@chalapuk.pl>
2019-03-24 16:18:31 +0100

    + initial data preload in ClientRenderer

--- src/boot/ClientRenderer.ts
+++ src/boot/ClientRenderer.ts
@@ -1,6 +1,7 @@
 
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import * as ReactDomServer from 'react-dom/server';
 import { History, createBrowserHistory } from 'history';
 
 import { Router } from '../boot';
@@ -37,7 +38,33 @@ export class ClientRenderer {
     window.addEventListener('unload', unlisten);
 
     const initialPage = pages[this.history.location.pathname] || notFound;
-    resolve(initialPage);
+
+    // Need to wait for loaders added in .componentWillMount methods
+    // (same as server-side) in order to hydrate initial page without a warning.
+    this.preloadData(initialPage)
+      .then(() => resolve(initialPage))
+    ;
+  }
+
+  private async preloadData(page : Page) {
+    const { history, paramorph } = this;
+    const promises : Promise<any>[] = [];
+
+    const oldLoadData = paramorph.loadData;
+    paramorph.loadData = <T>(key : string, loader : () => Promise<T>) => {
+      promises.push(loader().then(value => paramorph.data[key] = value));
+      return Promise.resolve(null as any as T);
+    };
+
+    const { PageComponent } = await this.router.resolve(page.url);
+    const pageElement = React.createElement(PageComponent);
+    const props = { history, paramorph, page };
+    const app = React.createElement(ContextContainer, props, pageElement);
+
+    ReactDomServer.renderToString(app);
+    paramorph.loadData = oldLoadData;
+
+    await Promise.all(promises);
   }
 }
 
