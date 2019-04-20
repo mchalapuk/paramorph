92952fc359ce2dd31c151eb4ada0ce13455649c7
Maciej Chałapuk <maciej@chalapuk.pl>
2019-04-21 01:19:17 +0200

    ~ using setImmediate wihout global namespace

--- src/model/Paramorph.ts
+++ src/model/Paramorph.ts
@@ -182,7 +182,7 @@ export class Paramorph {
   }
 
   private notifyContentListeners(url : string) {
-    global.setImmediate(() => {
+    setImmediate(() => {
       const content = this.content[url] as React.ComponentType<{}>;
       this.contentListeners.forEach(listener => listener(url, content));
     });
